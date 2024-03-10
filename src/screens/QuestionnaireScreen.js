72837ccc0830b3dd0a1be3b42c2b6a39151ab4d3
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-03-10 15:12:47 +0100

    outsourced function and added navigation

--- src/screens/QuestionnaireScreen.js
+++ src/screens/QuestionnaireScreen.js
@@ -1,43 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, SafeAreaView, SectionList, Text } from "react-native";
 import { commonStyle } from "../styles/commonStyle";
 import { BottomNavigationView } from "../components/BottomNavigationView";
 import { overviewScreenRoute } from "../navigation/Navigation";
 import { questionnaireSections } from "../fhir-resources/Questionnaire";
-
-
-// Define renderItem function outside of the component's return statement
-const renderItem = ({ item }) => (
-    <View style={commonStyle.sectionContainer}>
-        <Text style={commonStyle.text}>{item}</Text>
-    </View>
-);
-
-// Define renderSectionHeader function outside of the component's return statement
-function renderSectionHeader({ section: { title } }) { 
-    return (
-    <View style={commonStyle.sectionHeader}>
-        <Text style={commonStyle.sectionHeaderText}>{title}</Text>
-    </View>
-)
-};
+import renderQuestionnaireItem from "../functions/renderQuestionnaireItem";
+import renderSectionHeader from "../functions/renderSectionHeader";
+import { useNavigation } from "@react-navigation/native";
 
 export default function QuestionnaireScreen() {
 
-    const currentQuestionnaireSection = [questionnaireSections[0]];
+    const navigation = useNavigation();
+    const [page, setPage] = useState(0);
+    const totalNumberOfPages = questionnaireSections.length - 1;
+
+    function handleNextButtonPress() {
+        // Check if it's the last page before navigating
+        if (page < totalNumberOfPages) {
+          setPage(page + 1);
+          console.log(currentQuestionnaireSection);
+
+        } else {
+          // Navigate to the next screen
+          console.log(currentQuestionnaireSection);
+          navigation.navigate(overviewScreenRoute);
+        }
+      };
+
+      function handleBackButtonPress() {
+        // Check if you are on the first page
+        if (page > 0) {
+          // If not, go back one page
+          setPage(page - 1);
+        } else {
+          // If on the first page, go back in navigation
+          navigation.goBack();
+        }
+      };
+
+    const currentQuestionnaireSection = [questionnaireSections[page]];
     console.log(currentQuestionnaireSection);
 
     return (
         <SafeAreaView style={commonStyle.body}>
             <View style={commonStyle.header}></View>
+            <View style={commonStyle.section}>
             <SectionList
-                sections={currentSection}
+                sections={currentQuestionnaireSection}
                 keyExtractor={(item, index) => item + index}
-                renderItem={renderItem}
+                renderItem={renderQuestionnaireItem}
                 renderSectionHeader={renderSectionHeader}
             />
+            </View>
             <View style={commonStyle.footer}>
-                <BottomNavigationView navigateTo={overviewScreenRoute} />
+                <BottomNavigationView primaryButtonPressed={handleNextButtonPress} secondaryButtonPressed={handleBackButtonPress} page={`${page + 1}`}/>
             </View>
         </SafeAreaView>
     );
