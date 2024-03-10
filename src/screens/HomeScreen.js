5f53e98978448a590c9da6be3c305b5fc19024c8
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-03-10 08:49:47 +0100

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, TouchableOpacity } from "react-native";
+import { Image, TouchableOpacity, StyleSheet } from "react-native";
 import { commonStyle } from "../styles/commonStyle";
 import { useNavigation } from "@react-navigation/native";
 import { selectLanguageScreenRoute } from "../navigation/Navigation";
@@ -9,12 +9,23 @@ export default function HomeScreen() {
     const navigation = useNavigation();
     return (
         <TouchableOpacity 
-            style={commonStyle.body} 
+            style={[commonStyle.body, styles.homePage]} 
             onPress={() => navigation.navigate(selectLanguageScreenRoute)}>
                 <Image
-                    style={commonStyle.body}
+                    style={styles.homeImage}
                     source={require("../assets/images/home-logo.png")}
                 />
         </TouchableOpacity>
     );
 };
+
+const styles = StyleSheet.create({
+    homeImage: {
+        resizeMode: "contain",
+        width: "75%",
+    },
+    homePage: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
