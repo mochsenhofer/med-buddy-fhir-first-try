c91bf85fb044e5dd6bd4a6bf3f59fe89ddaad6d7
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-03-10 08:50:04 +0100

    build Questionnaire

---
+++ src/fhir-resources/Questionnaire.js
@@ -0,0 +1,248 @@
+export const Questionnaire = {
+    resourceType: "Questionnaire",
+    id: "questionnaire-1",
+    subjectType: ["Patient"],
+    status: "active",
+    item: [
+        {
+            linkId: "1",
+            text: "Did you have one of these examinations before?",
+            type: "group",
+            item: [
+                {
+                    linkId: "1.1",
+                    text: "Computed Tomography (CT)",
+                    type: "choice",
+                    answerOption: [
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "N",
+                                display: "No"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "Y",
+                                display: "Yes"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "ASKU",
+                                display: "Unknown"
+                            }
+                        }
+                    ]
+                },
+                {
+                    linkId: "1.2",
+                    text: "Vascular X-ray (Angiography / Cardiac Catheter)",
+                    type: "choice",
+                    answerOption: [
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "N",
+                                display: "No"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "Y",
+                                display: "Yes"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "ASKU",
+                                display: "Unknown"
+                            }
+                        }
+                    ]
+                },
+                {
+                    linkId: "1.3",
+                    text: "Kidney X-ray (Urography)",
+                    type: "choice",
+                    answerOption: [
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "N",
+                                display: "No"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "Y",
+                                display: "Yes"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "ASKU",
+                                display: "Unknown"
+                            }
+                        }
+                    ]
+                },
+                {
+                    linkId: "1.4",
+                    text: "Leg vein imaging (phlebography)",
+                    type: "choice",
+                    answerOption: [
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "N",
+                                display: "No"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "Y",
+                                display: "Yes"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "ASKU",
+                                display: "Unknown"
+                            }
+                        }
+                    ]
+                },
+                // Placeholder for additional questions within this group
+            ],
+        },
+        {
+            linkId: "2",
+            text: "Did you experience any side-effects after the administration of contrast agents? If so, which ones?",
+            type: "group",
+            item: [
+                {
+                    linkId: "2.1",
+                    text: "Nausea/Vomiting/Suffocation",
+                    type: "choice",
+                    answerOption: [
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "N",
+                                display: "No"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "Y",
+                                display: "Yes"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "ASKU",
+                                display: "Unknown"
+                            }
+                        }
+                    ]
+                },
+                {
+                    linkId: "2.2",
+                    text: "Astma attack / shortness of breath",
+                    type: "choice",
+                    answerOption: [
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "N",
+                                display: "No"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "Y",
+                                display: "Yes"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "ASKU",
+                                display: "Unknown"
+                            }
+                        }
+                    ]
+                },
+                {
+                    linkId: "2.3",
+                    text: "Rash",
+                    type: "choice",
+                    answerOption: [
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "N",
+                                display: "No"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "Y",
+                                display: "Yes"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "ASKU",
+                                display: "Unknown"
+                            }
+                        }
+                    ]
+                },
+                {
+                    linkId: "2.4",
+                    text: "Seizures/unconsciousness",
+                    type: "choice",
+                    answerOption: [
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "N",
+                                display: "No"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "Y",
+                                display: "Yes"
+                            }
+                        },
+                        {
+                            valueCoding: {
+                                system: "http://terminology.hl7.org/CodeSystem/v2-0136",
+                                code: "ASKU",
+                                display: "Unknown"
+                            }
+                        }
+                    ]
+                },
+            ]
+        },
+        // Placeholder for additional groups or individual questions
+    ],
+};
