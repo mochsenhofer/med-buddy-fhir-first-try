cd420564a13e86c30d7bdc1cbf5540331779a7d5
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-03-10 11:33:46 +0100

    Update SelectLanguageScreen.js

--- src/screens/SelectLanguageScreen.js
+++ src/screens/SelectLanguageScreen.js
@@ -3,9 +3,11 @@ import { View, SafeAreaView } from "react-native";
 import { commonStyle } from "../styles/commonStyle";
 import { BottomNavigationView } from "../components/BottomNavigationView";
 import { registrationScreenRoute } from "../navigation/Navigation";
+import { Questionnaire } from "../fhir-resources/Questionnaire";
 
 
 export default function SelectLanguageScreen() {
+    console.log(JSON.stringify(Questionnaire));
     return (
         <SafeAreaView style={commonStyle.body}>
             <View style={commonStyle.header}></View>
