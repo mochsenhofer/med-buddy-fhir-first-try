d2105d5511909cda4c7442f2859ba1464e3deef8
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-03-10 14:28:12 +0100

    Update QuestionnaireScreen.js

--- src/screens/QuestionnaireScreen.js
+++ src/screens/QuestionnaireScreen.js
@@ -3,13 +3,8 @@ import { View, SafeAreaView, SectionList, Text } from "react-native";
 import { commonStyle } from "../styles/commonStyle";
 import { BottomNavigationView } from "../components/BottomNavigationView";
 import { overviewScreenRoute } from "../navigation/Navigation";
-import { Questionnaire } from "../fhir-resources/Questionnaire";
+import { questionnaireSections } from "../fhir-resources/Questionnaire";
 
-// Transform Questionnaire data for SectionList
-const transformedSections = Questionnaire.item.map((qItem) => ({
-    title: qItem.text, // Use 'text' as the title for the section
-    data: qItem.item ? qItem.item.map(subItem => subItem.text) : [], // Map sub-items' 'text' to data if they exist
-}));
 
 // Define renderItem function outside of the component's return statement
 const renderItem = ({ item }) => (
@@ -19,16 +14,18 @@ const renderItem = ({ item }) => (
 );
 
 // Define renderSectionHeader function outside of the component's return statement
-const renderSectionHeader = ({ section: { title } }) => (
+function renderSectionHeader({ section: { title } }) { 
+    return (
     <View style={commonStyle.sectionHeader}>
         <Text style={commonStyle.sectionHeaderText}>{title}</Text>
     </View>
-);
+)
+};
 
 export default function QuestionnaireScreen() {
 
-    const currentSection = [transformedSections[0]];
-    console.log(currentSection);
+    const currentQuestionnaireSection = [questionnaireSections[0]];
+    console.log(currentQuestionnaireSection);
 
     return (
         <SafeAreaView style={commonStyle.body}>
