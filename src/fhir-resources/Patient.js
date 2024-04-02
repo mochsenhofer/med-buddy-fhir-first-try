8f036443351cc00c8902db9a02bae5199b722eb8
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-04-02 13:55:02 +0200

    Create Patient.js

---
+++ src/fhir-resources/Patient.js
@@ -0,0 +1,12 @@
+export const Patient = {
+        resourceType: "Patient",
+        id: "patient-01",
+        gender: "male",
+        birthDate: "1905-08-23",
+        name: [
+            {
+                family: "Turbo",
+                given: ["Tom"],
+            },
+        ],
+    };
