7736ec7187147159f75d01bd88ff7d7a2fecfb50
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-04-04 12:40:02 +0200

    Update renderQuestionnaireItem.js

--- src/functions/renderQuestionnaireItem.js
+++ src/functions/renderQuestionnaireItem.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { commonStyle } from '../styles/commonStyle';
+import { View, Text, TextInput } from 'react-native';
+import { commonStyle, questionnaireItemStyle } from '../styles/commonStyle';
+import { QuestionnaireResponse } from '../fhir-resources/QuestionnaireResponse';
 
 // Define renderItem function outside of the component's return statement
 export default function renderQuestionnaireItem({ item }) {
     console.log(item)
     return (
     <View style={commonStyle.sectionContainer}>
-        <Text style={commonStyle.text}>{item.text}</Text>
-        {item.type === "choice" ? <Text >Radio</Text> : null}
-        {item.type === "integer" ? <Text >Text</Text> : null}
+        <Text style={questionnaireItemStyle.questionText}>{item.text}</Text>
+        {renderUserInput(item.type, item.maxLength, item.linkId)}
     </View>
 );
+};
+
+
+function renderUserInput(type, maxLength, linkId) {
+
+    console.log(QuestionnaireResponse.item)
+    
+    switch (type) {
+        case "integer":
+            return (
+                <TextInput
+                    style={questionnaireItemStyle.textInput}
+                    keyboardType="numeric"
+                    maxLength={maxLength}
+                />
+            );
+        case "string":
+            return (
+                <TextInput
+                    style={questionnaireItemStyle.textInput}
+                    keyboardType="default"
+                    maxLength={maxLength}
+                />
+            );
+        case "date":
+            return (
+                <TextInput
+                    style={questionnaireItemStyle.textInput}
+                    keyboardType="numeric"
+                    maxLength={maxLength}
+                />
+            );
+        case "choice":
+            return (
+                <Text>Radio</Text>
+            );
+        default:
+            return null;
+    }
 };
