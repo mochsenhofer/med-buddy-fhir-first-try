9c58145bb23d8e5a354b13eb6fefcab5923f30ba
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-04-05 10:28:25 +0200

    created component radioButtons

---
+++ src/components/RadioButtons.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { questionnaireItemStyle } from "../styles/commonStyle";
+
+function RadioButton({ selected, text, onPress }) { // Changed parameter to onPress for clarity
+    return (
+        <TouchableOpacity style={questionnaireItemStyle.radioButton} onPress={onPress}>
+            <Text style={questionnaireItemStyle.radioButtonText}>{text}</Text>
+        </TouchableOpacity>
+    );
+}
+
+export default function RadioButtons({ options }) {
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+    function radioButtonPressed(option) { // Now accepts option as an argument
+        setSelectedAnswer(option);
+        console.log("Selected Answer: ", option); // Log the option directly
+    }
+
+    return (
+        <View style={styles.radioButtonView}>
+            {options.map((option, index) => (
+                <RadioButton
+                    key={index}
+                    selected={selectedAnswer === option}
+                    text={option.valueCoding.code}
+                    onPress={() => radioButtonPressed(option.valueCoding.code)} // Correctly invoking the function on press
+                />
+            ))}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    radioButtonView: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "80%",
+    },
+});
