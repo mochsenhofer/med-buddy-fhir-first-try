38a0db51ad04de5085d151df90d5fd16572f64d0
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-04-06 10:45:25 +0200

    Update RegistrationScreen.js

--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -3,9 +3,11 @@ import { View, SafeAreaView } from "react-native";
 import { commonStyle } from "../styles/commonStyle";
 import { BottomNavigationView } from "../components/BottomNavigationView";
 import { previewScreenRoute } from "../navigation/Navigation";
+import { Patient } from "../fhir-resources/Patient";
 
 
 export default function RegistrationScreen() {
+    console.log(JSON.stringify(Patient));
     return (
         <SafeAreaView style={commonStyle.body}>
             <View style={commonStyle.header}></View>
