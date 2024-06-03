71f0bd23329684ee896698c7760453f9494635c7
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-03 12:59:27 +0200

    updated Function

--- src/functions/renderQuestionnaireItem.js
+++ src/functions/renderQuestionnaireItem.js
@@ -1,30 +1,30 @@
 import React from "react";
 import { View, Text, TextInput } from "react-native";
 import { commonStyle, questionnaireItemStyle } from "../styles/commonStyle";
-import { QuestionnaireResponse } from "../fhir-resources/QuestionnaireResponse";
 
 // Define renderItem function outside of the component's return statement
-export default function renderQuestionnaireItem({ item: questionItem }) {
+export default function renderQuestionnaireItem({
+  qItem,
+  currentQuestionnaireSectionLinkId,
+  currentQuestionnaireResponseSectionLinkId,
+}) {
   return (
     <View style={commonStyle.sectionContainer}>
-      <Text style={questionnaireItemStyle.questionText}>
-        {questionItem.text}
-      </Text>
-      {renderUserInput({ questionItem })}
+      <Text style={questionnaireItemStyle.questionText}>{qItem.text}</Text>
+      {renderUserInput({ qItem })}
     </View>
   );
 }
 
-function renderUserInput({ questionItem }) {
-  //   console.log("questionItem: " + JSON.stringify(questionItem));
-  switch (questionItem.type) {
+function renderUserInput({ qItem }) {
+  switch (qItem.type) {
     case "integer":
       return (
         <TextInput
           style={questionnaireItemStyle.textInput}
           keyboardType="numeric"
           placeholder="Enter a number"
-          maxLength={questionItem.maxLength}
+          maxLength={qItem.maxLength}
         />
       );
     case "choice":
