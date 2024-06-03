7f610ab9382165afc20b92f678a7ae9df203b147
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-03 11:10:07 +0200

    formatting

--- src/functions/renderQuestionnaireItem.js
+++ src/functions/renderQuestionnaireItem.js
@@ -1,33 +1,31 @@
-import React from 'react';
-import { View, Text, TextInput } from 'react-native';
-import { commonStyle, questionnaireItemStyle } from '../styles/commonStyle';
-import { QuestionnaireResponse } from '../fhir-resources/QuestionnaireResponse';
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+import { commonStyle, questionnaireItemStyle } from "../styles/commonStyle";
+import { QuestionnaireResponse } from "../fhir-resources/QuestionnaireResponse";
 
 // Define renderItem function outside of the component's return statement
 export default function renderQuestionnaireItem({ item }) {
-    return (
+  return (
     <View style={commonStyle.sectionContainer}>
-        <Text style={questionnaireItemStyle.questionText}>{item.text}</Text>
-        {renderUserInput({ item })}
+      <Text style={questionnaireItemStyle.questionText}>{item.text}</Text>
+      {renderUserInput({ item })}
     </View>
-);
-};
+  );
+}
 
 function renderUserInput({ item }) {
-    console.log('linkID: ' + item.linkId);
-    switch(item.type) {
-        case 'integer':
-            return (
-                <TextInput
-                    style={questionnaireItemStyle.textInput}
-                    keyboardType='numeric'
-                    placeholder='Enter a number'
-                    maxLength={item.maxLength}
-                />
-            );
-        case 'choice':
-            return (
-                <Text>Choice</Text>
-            );
+  console.log("linkID: " + item.linkId);
+  switch (item.type) {
+    case "integer":
+      return (
+        <TextInput
+          style={questionnaireItemStyle.textInput}
+          keyboardType="numeric"
+          placeholder="Enter a number"
+          maxLength={item.maxLength}
+        />
+      );
+    case "choice":
+      return <Text>Choice</Text>;
+  }
 }
-};
--- src/screens/QuestionnaireScreen.js
+++ src/screens/QuestionnaireScreen.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { View, SafeAreaView, SectionList, KeyboardAvoidingView } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  SectionList,
+  KeyboardAvoidingView,
+} from "react-native";
 import { commonStyle } from "../styles/commonStyle";
 import { BottomNavigationView } from "../components/BottomNavigationView";
 import { overviewScreenRoute } from "../navigation/Navigation";
@@ -9,50 +14,52 @@ import renderSectionHeader from "../functions/renderSectionHeader";
 import { useNavigation } from "@react-navigation/native";
 
 export default function QuestionnaireScreen() {
+  const navigation = useNavigation();
+  const [page, setPage] = useState(0);
+  const totalNumberOfPages = questionnaireSections.length - 1;
 
-    const navigation = useNavigation();
-    const [page, setPage] = useState(0);
-    const totalNumberOfPages = questionnaireSections.length - 1;
+  function handleNextButtonPress() {
+    // Check if it's the last page before navigating
+    if (page < totalNumberOfPages) {
+      setPage(page + 1);
+    } else {
+      // Navigate to the next screen
+      navigation.navigate(overviewScreenRoute);
+    }
+  }
 
-    function handleNextButtonPress() {
-        // Check if it's the last page before navigating
-        if (page < totalNumberOfPages) {
-          setPage(page + 1);
+  function handleBackButtonPress() {
+    // Check if you are on the first page
+    if (page > 0) {
+      // If not, go back one page
+      setPage(page - 1);
+    } else {
+      // If on the first page, go back in navigation
+      navigation.goBack();
+    }
+  }
 
-        } else {
-          // Navigate to the next screen
-          navigation.navigate(overviewScreenRoute);
-        }
-      };
+  const currentQuestionnaireSection = [questionnaireSections[page]];
 
-      function handleBackButtonPress() {
-        // Check if you are on the first page
-        if (page > 0) {
-          // If not, go back one page
-          setPage(page - 1);
-        } else {
-          // If on the first page, go back in navigation
-          navigation.goBack();
-        }
-      };
-
-    const currentQuestionnaireSection = [questionnaireSections[page]];
-
-    return (
-        <SafeAreaView style={commonStyle.body}>
-            <View style={commonStyle.header}></View>
-            <KeyboardAvoidingView behavior="padding" style={commonStyle.section}>
-            <SectionList
-                sections={currentQuestionnaireSection}
-                keyExtractor={(item, index) => item + index}
-                renderItem={renderQuestionnaireItem}
-                renderSectionHeader={renderSectionHeader}
-                stickySectionHeadersEnabled={false}
-            />
-            </KeyboardAvoidingView>
-            <View style={commonStyle.footer}>
-                <BottomNavigationView primaryButtonPressed={handleNextButtonPress} secondaryButtonPressed={handleBackButtonPress} page={`${page + 1}`}/>
-            </View>
-        </SafeAreaView>
-    );
-};
+  return (
+    <SafeAreaView style={commonStyle.body}>
+      <View style={commonStyle.header}></View>
+      <KeyboardAvoidingView behavior="padding" style={commonStyle.section}>
+        <SectionList
+          sections={currentQuestionnaireSection}
+          keyExtractor={(item, index) => item + index}
+          renderItem={renderQuestionnaireItem}
+          renderSectionHeader={renderSectionHeader}
+          stickySectionHeadersEnabled={false}
+        />
+      </KeyboardAvoidingView>
+      <View style={commonStyle.footer}>
+        <BottomNavigationView
+          primaryButtonPressed={handleNextButtonPress}
+          secondaryButtonPressed={handleBackButtonPress}
+          page={`${page + 1}`}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
