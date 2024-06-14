e76f10b3f69155e1ac07db17d799e3d325907111
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-14 14:16:25 +0200

    build reducer Functions

---
+++ src/store/patientActions.js
@@ -0,0 +1,35 @@
+export function changePatientsGivenName() {
+  return {
+    type: "CHANGE_PATIENTS_GIVEN_NAME",
+  };
+}
+
+export function changePatientsFamilyName() {
+  return {
+    type: "CHANGE_PATIENTS_FAMILY_NAME",
+  };
+}
+
+export function changePatientsBirthdate() {
+  return {
+    type: "CHANGE_PATIENTS_BIRTHDATE",
+  };
+}
+
+export function changePatientsGender() {
+  return {
+    type: "CHANGE_PATIENTS_GENDER",
+  };
+}
+
+export function changePatientsInsuranceNumber() {
+  return {
+    type: "CHANGE_PATIENTS_INSURANCE_NUMBER",
+  };
+}
+
+export function changePatientsLanguage() {
+  return {
+    type: "CHANGE_PATIENTS_LANGUAGE",
+  };
+}
