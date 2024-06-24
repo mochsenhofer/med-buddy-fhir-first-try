7eec7b2c36594a5c7613d08b0b0b8979301604b5
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-24 18:19:14 +0200

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, createStore } from "redux";
 import patientReducer from "./patientReducer";
+import questionnaireResponseReducer from "./questionnaireResponseReducer";
 
 const rootReducer = combineReducers({
   patientData: patientReducer,
+  questionnaireResponseData: questionnaireResponseReducer,
 });
 
 export const store = createStore(rootReducer);
