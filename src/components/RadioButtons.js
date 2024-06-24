6827e001f104f2fff985e2195be83d12c9824bbc
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-24 11:14:34 +0200

    create new RadioButtons file

--- src/components/RadioButtons.js
+++ src/components/RadioButtons.js
@@ -1,41 +1,44 @@
 import React, { useState } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { questionnaireItemStyle } from "../styles/commonStyle";
 
-function RadioButton({ label, onPress, isSelected }) {
+function RadioButton({ text, selected, onSelect }) {
   return (
     <TouchableOpacity
       style={[
         questionnaireItemStyle.radioButton,
-        isSelected && questionnaireItemStyle.radioButtonSelected,
+        selected && questionnaireItemStyle.radioButtonSelected,
       ]}
-      onPress={onPress}
+      onPress={onSelect}
     >
-      <Text style={questionnaireItemStyle.radioButtonText}>{label}</Text>
+      <Text
+        style={[
+          questionnaireItemStyle.radioButtonText,
+          selected && questionnaireItemStyle.radioButtonSelectedText,
+        ]}
+      >
+        {text}
+      </Text>
     </TouchableOpacity>
   );
 }
 
-export default function RadioButtons({ options, qItem, qrItem, updateState }) {
-  const [selectedValue, setSelectedValue] = useState(
-    qrItem.answer[0].valueCoding.code
-  );
+export default function RadioButtons({ options, onSelect }) {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+    onSelect(option);
+  };
 
   return (
-    <View style={questionnaireItemStyle.radioButtonView} key={qItem.linkId}>
-      {options.map((option, index) => (
+    <View style={questionnaireItemStyle.radioButtonView}>
+      {options.map((option) => (
         <RadioButton
-          key={index + "," + qItem.linkId}
-          label={option.valueCoding.code}
-          isSelected={selectedValue === option.valueCoding.code}
-          onPress={() => {
-            const newValue = option.valueCoding.code;
-            setSelectedValue(newValue);
-            updateState(newValue, qrItem.linkId, qItem.type);
-            console.log(
-              JSON.stringify(<RadioButton key={index + "," + qItem.linkId} />)
-            );
-          }}
+          key={option.text}
+          text={option.text}
+          selected={option.text === selectedOption}
+          onSelect={() => handleSelect(option.text)}
         />
       ))}
     </View>
