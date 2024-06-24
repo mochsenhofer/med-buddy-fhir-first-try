749637b72be278d3e67b5a4f0be80744c1d38a71
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-24 18:19:05 +0200

    Create questionnaireResponseActions.js

---
+++ src/store/questionnaireResponseActions.js
@@ -0,0 +1,17 @@
+export function changeQuestionnaireResponseAnswerValueCoding() {
+  return {
+    type: "CHANGE_QUESTIONNAIRE_RESPONSE_ANSWER_VALUE_CODING",
+  };
+}
+
+export function changeQuestionnaireResponseAnswerValueInteger() {
+  return {
+    type: "CHANGE_QUESTIONNAIRE_RESPONSE_ANSWER_VALUE_INTEGER",
+  };
+}
+
+export function changeQuestionnaireResponseAnswerValueString() {
+  return {
+    type: "CHANGE_QUESTIONNAIRE_RESPONSE_ANSWER_VALUE_STRING",
+  };
+}
