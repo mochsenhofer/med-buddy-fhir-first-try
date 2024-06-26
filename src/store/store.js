a9499ebd838e6935b91658b16ec8cc5dcf5f7c52
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-26 16:15:14 +0200

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import patientReducer from "./patientReducer";
+import questionnaireResponseReducer from "./questionnaireResponseReducer";
 
 export const store = configureStore({
   reducer: {
     patient: patientReducer,
+    questionnaireResponse: questionnaireResponseReducer,
   },
 });
