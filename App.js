9c640a112b895d1b8a5f5fe409bd4058cb75e80d
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-26 12:17:20 +0200

    add redux provider

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
+import React from "react";
 import MedBuddyStackNavigation from "./src/navigation/Navigation";
+import { store } from "./src/store/store";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <MedBuddyStackNavigation />
+    <Provider store={store}>
+      <MedBuddyStackNavigation />
+    </Provider>
   );
-};
+}
