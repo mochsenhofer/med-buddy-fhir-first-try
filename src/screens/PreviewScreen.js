184aaa9853012fb5d0af7a4c8f19aaf5efee3d9a
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-26 12:18:47 +0200

    Update PreviewScreen.js

--- src/screens/PreviewScreen.js
+++ src/screens/PreviewScreen.js
@@ -1,18 +1,27 @@
 import React from "react";
-import { View, SafeAreaView } from "react-native";
+import { View, SafeAreaView, Text } from "react-native";
 import { commonStyle } from "../styles/commonStyle";
 import { BottomNavigationView } from "../components/BottomNavigationView";
-import { informationScreenRoute, videoScreenRoute } from "../navigation/Navigation";
-
+import {
+  informationScreenRoute,
+  videoScreenRoute,
+} from "../navigation/Navigation";
+import { useSelector } from "react-redux";
 
 export default function PreviewScreen() {
-    return (
-        <SafeAreaView style={commonStyle.body}>
-            <View style={commonStyle.header}></View>
-            <View style={commonStyle.section}></View>
-            <View style={commonStyle.footer}>
-                <BottomNavigationView navigateTo={informationScreenRoute}/>
-            </View>
-        </SafeAreaView>
-    );
+  const registeredPatient = useSelector((state) => state.patient);
+
+  return (
+    <SafeAreaView style={commonStyle.body}>
+      <View style={commonStyle.header}>
+        <Text>Preview + {registeredPatient.name[0].given}</Text>
+      </View>
+      <View style={commonStyle.section}>
+        <Text>{JSON.stringify(registeredPatient)}</Text>
+      </View>
+      <View style={commonStyle.footer}>
+        <BottomNavigationView navigateTo={informationScreenRoute} />
+      </View>
+    </SafeAreaView>
+  );
 }
