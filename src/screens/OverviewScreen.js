c3aa27e99214759f7c4737890e6a855f694320e0
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-26 16:14:00 +0200

    check if reducer is working

--- src/screens/OverviewScreen.js
+++ src/screens/OverviewScreen.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { View, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import { commonStyle } from "../styles/commonStyle";
 import { BottomNavigationView } from "../components/BottomNavigationView";
 import { agreementScreenRoute } from "../navigation/Navigation";
+import { useSelector } from "react-redux";
 
 export default function OverviewScreen() {
+  const updatedQuestionnaireResponse = useSelector(
+    (state) => state.questionnaireResponse
+  );
+
   return (
     <SafeAreaView style={commonStyle.body}>
-      <View style={commonStyle.header}></View>
-      <View style={commonStyle.section}></View>
+      <View style={commonStyle.header}>
+        <Text>
+          Hello +
+          {updatedQuestionnaireResponse.item[0].item[0].answer.valueInteger}
+        </Text>
+      </View>
+      <View style={commonStyle.section}>
+        <Text>{JSON.stringify(updatedQuestionnaireResponse)}</Text>
+      </View>
       <View style={commonStyle.footer}>
         <BottomNavigationView navigateTo={agreementScreenRoute} />
       </View>
