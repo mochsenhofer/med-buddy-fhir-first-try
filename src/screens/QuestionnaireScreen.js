068f8ce0e7f1742de020cd2a007eab968ee3041b
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-27 09:36:04 +0200

    Update QuestionnaireScreen.js

--- src/screens/QuestionnaireScreen.js
+++ src/screens/QuestionnaireScreen.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
   KeyboardAvoidingView,
   SafeAreaView,
@@ -12,29 +12,144 @@ import { BottomNavigationView } from "../components/BottomNavigationView";
 import renderSectionHeader from "../functions/renderSectionHeader";
 import { overviewScreenRoute } from "../navigation/Navigation";
 import { commonStyle } from "../styles/commonStyle";
-import { updateProperty } from "../store/questionnaireResponseReducer";
+import {
+  updateValueInteger,
+  updateValueString,
+  updateValueCoding,
+} from "../store/questionnaireResponseReducer";
 import { questionnaireSections } from "../fhir-resources/Questionnaire";
 import { questionnaireItemStyle } from "../styles/commonStyle";
+import RadioButtons from "../components/RadioButtons";
+import { useNavigation } from "@react-navigation/native";
 
 export default function QuestionnaireScreen() {
+  const [page, setPage] = useState(0);
+  const navigation = useNavigation();
+  const totalNumberOfPages = questionnaireSections.length - 1;
+  const currentQuestionnaireSection = [questionnaireSections[page]];
   const questionnaireResponseInProgress = useSelector(
-    (state) => state.questionnaireResponse
+    (state) => state.questionnaireResponse || {}
   );
   const dispatch = useDispatch();
 
-  function renderQuestionnaireItem({ item }) {
+  function handleNextButtonPress() {
+    // Check if it's the last page before navigating
+    if (page < totalNumberOfPages) {
+      setPage(page + 1);
+    } else {
+      // Navigate to the next screen
+      navigation.navigate(overviewScreenRoute);
+    }
+  }
+
+  function handleBackButtonPress() {
+    // Check if you are on the first page
+    if (page > 0) {
+      // If not, go back one page
+      setPage(page - 1);
+    } else {
+      // If on the first page, go back in navigation
+      navigation.goBack();
+    }
+  }
+
+  function findResponseItem(linkId, items) {
+    for (const item of items) {
+      if (item.linkId === linkId) {
+        return item;
+      }
+      if (item.item) {
+        const foundItem = findResponseItem(linkId, item.item);
+        if (foundItem) {
+          return foundItem;
+        }
+      }
+    }
+    return null;
+  }
+
+  function getValueByLinkId(item) {
+    const responseItem = findResponseItem(
+      item.linkId,
+      questionnaireResponseInProgress.item || []
+    );
+    if (responseItem && responseItem.answer && responseItem.answer[0]) {
+      switch (item.type) {
+        case "integer":
+          return responseItem.answer[0].valueInteger
+            ? responseItem.answer[0].valueInteger.toString()
+            : "";
+        case "string":
+          return responseItem.answer[0].valueString || "";
+        case "choice":
+          return responseItem.answer[0].valueCoding
+            ? responseItem.answer[0].valueCoding.code
+            : "";
+        default:
+          return "";
+      }
+    }
+    return "";
+  }
+
+  function renderUserInputQuestionnaire({ item }) {
+    const value = getValueByLinkId(item);
+
+    switch (item.type) {
+      case "integer":
+        return (
+          <TextInput
+            style={questionnaireItemStyle.textInput}
+            maxLength={item.maxLength}
+            keyboardType="numeric"
+            value={value}
+            onChangeText={(text) => {
+              dispatch(
+                updateValueInteger({
+                  linkId: item.linkId,
+                  value: parseInt(text),
+                })
+              );
+            }}
+          />
+        );
+      case "string":
+        return (
+          <TextInput
+            style={questionnaireItemStyle.textInput}
+            keyboardType="default"
+            value={value}
+            onChangeText={(text) => {
+              dispatch(updateValueString({ linkId: item.linkId, value: text }));
+            }}
+          />
+        );
+      case "choice":
+        return (
+          <RadioButtons
+            options={item.answerOption}
+            currentValue={value}
+            onSelect={(option) =>
+              dispatch(
+                updateValueCoding({ linkId: item.linkId, value: option })
+              )
+            }
+          />
+        );
+      default:
+        return null;
+    }
+  }
+
+  function renderQuestionnaireAndQuestionnaireResponseItem({ item }) {
     return (
       <View style={questionnaireItemStyle.questionContainer}>
         <Text style={questionnaireItemStyle.questionText}>{item.text}</Text>
+        {renderUserInputQuestionnaire({ item })}
       </View>
     );
   }
 
-  // onChange: (text) => {
-  //   dispatch(updateProperty({ linkId: "1.1", value: parseInt(text) }));
-  // },
-
-  console.log(JSON.stringify(questionnaireSections));
   return (
     <SafeAreaView style={commonStyle.body}>
       <View style={commonStyle.header}>
@@ -42,15 +157,19 @@ export default function QuestionnaireScreen() {
       </View>
       <KeyboardAvoidingView style={commonStyle.section} behavior="padding">
         <SectionList
-          sections={questionnaireSections}
+          sections={currentQuestionnaireSection}
           keyExtractor={(item) => item.linkId}
           renderSectionHeader={renderSectionHeader}
-          renderItem={renderQuestionnaireItem}
+          renderItem={renderQuestionnaireAndQuestionnaireResponseItem}
           stickySectionHeadersEnabled={false}
         />
       </KeyboardAvoidingView>
       <View style={commonStyle.footer}>
-        <BottomNavigationView navigateTo={overviewScreenRoute} />
+        <BottomNavigationView
+          primaryButtonPressed={handleNextButtonPress}
+          secondaryButtonPressed={handleBackButtonPress}
+          page={`${page + 1}`}
+        />
       </View>
     </SafeAreaView>
   );
