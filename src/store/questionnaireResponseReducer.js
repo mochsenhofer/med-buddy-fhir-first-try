29aaa29ea0d81645625e220ef86bdced1797c684
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-06-27 08:42:52 +0200

    Update questionnaireResponseReducer.js

--- src/store/questionnaireResponseReducer.js
+++ src/store/questionnaireResponseReducer.js
@@ -7,7 +7,7 @@ export const questionnaireResponseReducer = createSlice({
   name: "questionnaireResponse",
   initialState,
   reducers: {
-    updateProperty: (state, action) => {
+    updateValueInteger: (state, action) => {
       const { linkId, value } = action.payload;
       // Find the item with the matching linkId
       const findAndUpdateItem = (items) => {
@@ -25,10 +25,47 @@ export const questionnaireResponseReducer = createSlice({
 
       findAndUpdateItem(state.item);
     },
+    updateValueString: (state, action) => {
+      const { linkId, value } = action.payload;
+      // Find the item with the matching linkId
+      const findAndUpdateItem = (items) => {
+        for (let item of items) {
+          if (item.linkId === linkId && item.answer && item.answer[0]) {
+            item.answer[0].valueString = value;
+            return true;
+          }
+          if (item.item && findAndUpdateItem(item.item)) {
+            return true;
+          }
+        }
+        return false;
+      };
+
+      findAndUpdateItem(state.item);
+    },
+    updateValueCoding: (state, action) => {
+      const { linkId, value } = action.payload;
+      // Find the item with the matching linkId
+      const findAndUpdateItem = (items) => {
+        for (let item of items) {
+          if (item.linkId === linkId && item.answer && item.answer[0]) {
+            item.answer[0].valueCoding = value;
+            return true;
+          }
+          if (item.item && findAndUpdateItem(item.item)) {
+            return true;
+          }
+        }
+        return false;
+      };
+
+      findAndUpdateItem(state.item);
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { updateProperty } = questionnaireResponseReducer.actions;
+export const { updateValueInteger, updateValueString, updateValueCoding } =
+  questionnaireResponseReducer.actions;
 
 export default questionnaireResponseReducer.reducer;
