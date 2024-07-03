cf08a37a8fdc746600788a00681937fd760a9a82
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-03 12:32:15 +0200

    change Questionnaire Structure

--- src/fhir-resources/Questionnaire.js
+++ src/fhir-resources/Questionnaire.js
@@ -1,531 +1,77 @@
+// Your Questionnaire resource
 export const Questionnaire = {
   resourceType: "Questionnaire",
   id: "questionnaire-1",
   status: "active",
   item: [
     {
-      linkId: "1",
-      text: "Please answer the following questions to the best of your knowledge.",
+      linkId: "information",
       type: "group",
+      readOnly: true,
       item: [
         {
-          linkId: "1.1",
-          text: "Size (cm)",
-          type: "integer",
-          maxLength: 3,
-        },
-        {
-          linkId: "1.2",
-          text: "Weight (kg)",
-          type: "integer",
-          maxLength: 3,
-        },
-        {
-          linkId: "1.3",
-          text: "Is there a possibility of pregnancy?",
-          type: "choice",
-          answerOption: [
+          linkId: "i.1",
+          text: "Informationtext 1",
+          type: "group",
+          item: [
             {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
+              linkId: "i.1.1",
+              text: "Some information text 1",
+              type: "display",
             },
           ],
         },
       ],
     },
     {
-      linkId: "2",
-      text: "Did you have one of these examinations before?",
+      linkId: "questionnaire",
       type: "group",
+      readOnly: false,
       item: [
         {
-          linkId: "2.1",
-          text: "Computer tomography (CT)",
-          type: "choice",
-          answerOption: [
+          linkId: "q.1",
+          text: "Please answer the following questions",
+          type: "group",
+          item: [
             {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
+              linkId: "q.1.1",
+              text: "Size (cm)",
+              type: "integer",
+              maxLength: 3,
             },
             {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
+              linkId: "q.1.2",
+              text: "Weight (kg)",
+              type: "integer",
+              maxLength: 3,
             },
             {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "2.2",
-          text: "Vascular X-ray (Angiography / Cardiac Catheter)",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "2.3",
-          text: "Kidney X-ray (Urography)",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "2.4",
-          text: "Leg vein imaging (phlebography)",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-      ],
-    },
-    {
-      linkId: "3",
-      text: "Did you experience any side-effects after the administration of contrast agents? If so, which ones?",
-      type: "group",
-      item: [
-        {
-          linkId: "3.1",
-          text: "Nausea/Vomiting/Suffocation",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "3.2",
-          text: "Astma attack / shortness of breath",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "3.3",
-          text: "Rash",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "3.4",
-          text: "Seizures / unconsciousness",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "3.5",
-          text: "Shivering attacks",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-      ],
-    },
-    {
-      linkId: "4",
-      text: "Do you suffer from any of the following medical conditions?",
-      type: "group",
-      item: [
-        {
-          linkId: "4.1",
-          text: "Allergies that require treatment?",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "4.2",
-          text: "of the heart?",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "4.3",
-          text: "of the kidney / adrenal gland?",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "4.4",
-          text: "of the thyroid gland?",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "4.5",
-          text: "Diabetes?",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-        {
-          linkId: "4.6",
-          text: "Myasthenia gravis? (specific muscle disease)",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
-            },
-          ],
-        },
-      ],
-    },
-    {
-      linkId: "5",
-      text: "Please answer the following questions to the best of your knowledge.",
-      type: "group",
-      item: [
-        {
-          linkId: "5.1",
-          text: "If yes, which diabetes medications do you take?",
-          type: "string",
-        },
-        {
-          linkId: "5.2",
-          text: "Do you carry a pacemaker?",
-          type: "choice",
-          answerOption: [
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "Y",
-                display: "Yes",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "N",
-                display: "No",
-              },
-            },
-            {
-              valueCoding: {
-                system: "http://terminology.hl7.org/CodeSystem/v2-0532",
-                code: "ASKU",
-                display: "asked but unknown",
-              },
+              linkId: "q.1.3",
+              text: "Is there a possibility of pregnancy?",
+              type: "choice",
+              answerOption: [
+                {
+                  valueCoding: {
+                    system: "http://terminology.hl7.org/CodeSystem/v2-0532",
+                    code: "Y",
+                    display: "Yes",
+                  },
+                },
+                {
+                  valueCoding: {
+                    system: "http://terminology.hl7.org/CodeSystem/v2-0532",
+                    code: "N",
+                    display: "No",
+                  },
+                },
+                {
+                  valueCoding: {
+                    system: "http://terminology.hl7.org/CodeSystem/v2-0532",
+                    code: "ASKU",
+                    display: "asked but unknown",
+                  },
+                },
+              ],
             },
           ],
         },
@@ -536,8 +82,25 @@ export const Questionnaire = {
 
 // Transform the Questionnaire resource into a format that can be used by the SectionList component
 
-export const questionnaireSections = Questionnaire.item.map((qItem) => ({
-  title: qItem.text, // Use 'text' as the title for the section
-  linkId: qItem.linkId, // Use 'linkId' as the key for the section
-  data: qItem.item ? qItem.item : [], // Pass the full item objects if they exist
-}));
+const sectionWithLinkIdQuestionnaire = Questionnaire.item.find(
+  (item) => item.linkId === "questionnaire" // Correct equality check
+);
+
+console.log(
+  "sectionWithLinkIdQuestionnaire: " +
+    JSON.stringify(sectionWithLinkIdQuestionnaire, null, 2)
+);
+
+export const questionnaireSections = sectionWithLinkIdQuestionnaire.item.map(
+  (qItem) => ({
+    title: qItem.text, // Use 'text' as the title for the section
+    linkId: qItem.linkId, // Use 'linkId' as the key for the section
+    data: qItem.item ? qItem.item : [], // Pass the full item objects if they exist
+  })
+);
+
+console.log(
+  "questionnaireSections: " + JSON.stringify(questionnaireSections, null, 2)
+);
+
+// Now you can use 'questionnaireSections' with the SectionList component
