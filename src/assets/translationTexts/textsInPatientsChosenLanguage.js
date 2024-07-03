4742f97fcd970a7fbfbd76925fddd173ade7345b
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-03 22:03:56 +0200

    Update textsInPatientsChosenLanguage.js

--- src/assets/translationTexts/textsInPatientsChosenLanguage.js
+++ src/assets/translationTexts/textsInPatientsChosenLanguage.js
@@ -1,6 +1,8 @@
 const textsInPatientsChosenLanguage = {
   en: {
-    patient: {
+    registrationScreen: {
+      heading: "Welcome!",
+      subheading: "Please enter your personal information",
       "p.1": "Personal Information",
       "p.1.1": "Given Name",
       "p.1.2": "Family Name",
@@ -11,63 +13,74 @@ const textsInPatientsChosenLanguage = {
       "p.female": "Female",
       "p.other": "Other",
     },
-    information: {
-      "i.1": "Dear Patient!",
-      "i.1.1":
-        "You were referred for computer tomography (CT) by your attending doctor. For your information, please read this text and answer the questions on page 2. Afterwards, you will be informed about all important facts and have the opportunity to ask questions.",
-      "i.2": "What is computer tomography?",
-      "i.2.1":
-        "Computer tomography (CT) is a special X-ray examination used to make cross-sectional images of the body. This way, important information on the location of the disease centres can be obtained. This is of crucial importance for further treatment.",
-      "i.3": "How is the examination carried out?",
-      "i.3.1":
-        "The examination takes about 10-15 minutes. During the examination, you lie on a special examination table that moves through the opening on the CT device. Throughout the examination you are monitored by qualified personnel and connected with them via an intercom system.",
-      "i.4": "What do I have to do?",
-      "i.4.1":
-        "In order to achieve optimal image quality, we ask you to remain still during the entire examination. In some cases, we may ask you to follow instructions (e.g.: inhale, exhale, hold breath, do not swallow, etc.).",
-      "i.5": "Risk of X-rays:",
-      "i.5.1":
-        "In very rare cases, X-rays in high doses can contribute to tumour development. In general, the risk of CT X-rays is very low and depends on the type and number of CT and X-ray examinations performed. You have been referred to this CT scan by your attending doctor because this examination is important for diagnosis and the information cannot be obtained by another examination.",
-      "i.6": "Why contrast agent?",
-      "i.6.1":
-        "Contrast agents can increase the information significance of many CT scans and help your doctors to better recognize pathological changes. Contrast agents are administered as infusions into an arm vein during the examination.",
-      "i.7": "Can there be complications?",
-      "i.7.1":
-        "As with any injection, pain, bruising and, very rarely, infection can occur at the injection site. During the application, many people experience warmth and a bitter taste in their mouths. Scientific studies show that more than 99% of all patients tolerate the contrast agents used well. Seldom (about 1 in 400) there is a slight drop in blood pressure and thus slight discomfort such as nausea or pain. Very rarely (about 1 in 10,000) there are severe side effects such as shortness of breath, drop in blood pressure, cardiac arrhythmia or cramps. As with almost any medical procedure, life-threatening complications are possible, but extremely unlikely. If you experience any side effects, immediate medical assistance is always available.",
-      "i.7.2":
-        "In rare cases, contrast agent may leak into the arm at the injection site. This leads to painful swelling that can sometimes last for days and must be treated. If you feel pain or swelling of your arm during the examination, please notify the medical technician who carries out the examination immediately - even during the examination.",
+    previewSrceen: {
+      heading: "Welcome to your Computed Tomography Examination",
+      subheading:
+        "Please take a seat in the waiting area. During the waiting time, you can watch an educational video or read information material. Then please fill out the questionnaire",
+      watchVideo: "Watch Video",
+      readInformation: "Read Information",
     },
-    questionnaire: {
-      "q.1":
-        "Please answer the following questions to the best of your knowledge.",
-      "q.1.1": "Size (cm)",
-      "q.1.2": "Weight (kg)",
-      "q.1.3": "Is there a possibility of pregnancy?",
-      "q.2": "Did you have one of these examinations before?",
-      "q.2.1": "Computer tomography (CT)",
-      "q.2.2": "Vascular X-ray (Angiography / Cardiac Catheter)",
-      "q.2.3": "Kidney X-ray (Urography)",
-      "q.2.4": "Leg vein imaging (phlebography)",
-      "q.3":
-        "Did you experience any side-effects after the administration of contrast agents? If so, which ones?",
-      "q.3.1": "Nausea/Vomiting/Suffocation",
-      "q.3.2": "Asthma attack / shortness of breath",
-      "q.3.3": "Rash",
-      "q.3.4": "Seizures / unconsciousness",
-      "q.3.5": "Shivering attacks",
-      "q.4": "Do you suffer from any of the following medical conditions?",
-      "q.4.1": "Allergies that require treatment?",
-      "q.4.2": "of the heart?",
-      "q.4.3": "of the kidney / adrenal gland?",
-      "q.4.4": "of the thyroid gland?",
-      "q.4.5": "Diabetes?",
-      "q.4.6": "Myasthenia gravis? (specific muscle disease)",
-      "q.5":
-        "Please answer the following questions to the best of your knowledge.",
-      "q.5.1": "If yes, which diabetes medications do you take?",
-      "q.5.2": "Do you carry a pacemaker?",
-      "q.yes": "Yes",
-      "q.no": "No",
-      "q.unknown": "asked but unknown",
+    informationScreen: {
+      information: {
+        "i.1": "Dear Patient!",
+        "i.1.1":
+          "You were referred for computer tomography (CT) by your attending doctor. For your information, please read this text and answer the questions on page 2. Afterwards, you will be informed about all important facts and have the opportunity to ask questions.",
+        "i.2": "What is computer tomography?",
+        "i.2.1":
+          "Computer tomography (CT) is a special X-ray examination used to make cross-sectional images of the body. This way, important information on the location of the disease centres can be obtained. This is of crucial importance for further treatment.",
+        "i.3": "How is the examination carried out?",
+        "i.3.1":
+          "The examination takes about 10-15 minutes. During the examination, you lie on a special examination table that moves through the opening on the CT device. Throughout the examination you are monitored by qualified personnel and connected with them via an intercom system.",
+        "i.4": "What do I have to do?",
+        "i.4.1":
+          "In order to achieve optimal image quality, we ask you to remain still during the entire examination. In some cases, we may ask you to follow instructions (e.g.: inhale, exhale, hold breath, do not swallow, etc.).",
+        "i.5": "Risk of X-rays:",
+        "i.5.1":
+          "In very rare cases, X-rays in high doses can contribute to tumour development. In general, the risk of CT X-rays is very low and depends on the type and number of CT and X-ray examinations performed. You have been referred to this CT scan by your attending doctor because this examination is important for diagnosis and the information cannot be obtained by another examination.",
+        "i.6": "Why contrast agent?",
+        "i.6.1":
+          "Contrast agents can increase the information significance of many CT scans and help your doctors to better recognize pathological changes. Contrast agents are administered as infusions into an arm vein during the examination.",
+        "i.7": "Can there be complications?",
+        "i.7.1":
+          "As with any injection, pain, bruising and, very rarely, infection can occur at the injection site. During the application, many people experience warmth and a bitter taste in their mouths. Scientific studies show that more than 99% of all patients tolerate the contrast agents used well. Seldom (about 1 in 400) there is a slight drop in blood pressure and thus slight discomfort such as nausea or pain. Very rarely (about 1 in 10,000) there are severe side effects such as shortness of breath, drop in blood pressure, cardiac arrhythmia or cramps. As with almost any medical procedure, life-threatening complications are possible, but extremely unlikely. If you experience any side effects, immediate medical assistance is always available.",
+        "i.7.2":
+          "In rare cases, contrast agent may leak into the arm at the injection site. This leads to painful swelling that can sometimes last for days and must be treated. If you feel pain or swelling of your arm during the examination, please notify the medical technician who carries out the examination immediately - even during the examination.",
+      },
+    },
+    questionnaireScreen: {
+      questionnaire: {
+        "q.1":
+          "Please answer the following questions to the best of your knowledge.",
+        "q.1.1": "Size (cm)",
+        "q.1.2": "Weight (kg)",
+        "q.1.3": "Is there a possibility of pregnancy?",
+        "q.2": "Did you have one of these examinations before?",
+        "q.2.1": "Computer tomography (CT)",
+        "q.2.2": "Vascular X-ray (Angiography / Cardiac Catheter)",
+        "q.2.3": "Kidney X-ray (Urography)",
+        "q.2.4": "Leg vein imaging (phlebography)",
+        "q.3":
+          "Did you experience any side-effects after the administration of contrast agents? If so, which ones?",
+        "q.3.1": "Nausea/Vomiting/Suffocation",
+        "q.3.2": "Asthma attack / shortness of breath",
+        "q.3.3": "Rash",
+        "q.3.4": "Seizures / unconsciousness",
+        "q.3.5": "Shivering attacks",
+        "q.4": "Do you suffer from any of the following medical conditions?",
+        "q.4.1": "Allergies that require treatment?",
+        "q.4.2": "of the heart?",
+        "q.4.3": "of the kidney / adrenal gland?",
+        "q.4.4": "of the thyroid gland?",
+        "q.4.5": "Diabetes?",
+        "q.4.6": "Myasthenia gravis? (specific muscle disease)",
+        "q.5":
+          "Please answer the following questions to the best of your knowledge.",
+        "q.5.1": "If yes, which diabetes medications do you take?",
+        "q.5.2": "Do you carry a pacemaker?",
+        "q.yes": "Yes",
+        "q.no": "No",
+        "q.unknown": "asked but unknown",
+      },
     },
     navigationButtons: {
       "n.next": "Next",
