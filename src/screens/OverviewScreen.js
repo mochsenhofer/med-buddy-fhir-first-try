390ff200f70b6cf26aa2e0fb03e77949a0e52911
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-04 16:31:39 +0200

    Update OverviewScreen.js

--- src/screens/OverviewScreen.js
+++ src/screens/OverviewScreen.js
@@ -1,77 +1,230 @@
 import React from "react";
 import { View, Text, SafeAreaView, SectionList } from "react-native";
-import { commonStyle, fontStyle, overviewStyle } from "../styles/commonStyle";
+import { commonStyle, fontStyle } from "../styles/commonStyle";
 import { BottomNavigationView } from "../components/BottomNavigationView";
 import { agreementScreenRoute } from "../navigation/Navigation";
 import { useSelector } from "react-redux";
-import useQuestionnaireData from "../hooks/useQuestionnaireData";
+import { textsInPatientsChosenLanguage } from "../assets/translationTexts/textsInPatientsChosenLanguage";
 
 export default function OverviewScreen() {
   const updatedQuestionnaireResponse = useSelector(
     (state) => state.questionnaireResponse
   );
+  const registeredPatient = useSelector((state) => state.patient);
+  const language = registeredPatient.communication[0].language.coding[0].code;
+  const translatedQuestionnaireTexts =
+    textsInPatientsChosenLanguage[language].questionnaireScreen.questionnaire;
+  const translatedPatientTexts =
+    textsInPatientsChosenLanguage[language].registrationScreen;
 
-  const { questionnaireSections, Questionnaire } = useQuestionnaireData();
-
-  const updatedQuestionnaireResponseSections =
-    updatedQuestionnaireResponse.item.map((uqrsItem) => ({
-      title: uqrsItem.text, // Use 'text' as the title for the section
-      data: uqrsItem.item, // Use 'item' as the data for the section
-    }));
-
-  function findQuestionnaireItemByLinkId(items, linkId) {
-    for (const item of items) {
-      if (item.linkId === linkId) {
-        return item;
-      }
-      if (item.item) {
-        const nestedItem = findQuestionnaireItemByLinkId(item.item, linkId);
-        if (nestedItem) {
-          return nestedItem;
+  function findAnswerByLinkId(linkId) {
+    for (const item of updatedQuestionnaireResponse.item) {
+      for (const subItem of item.item || []) {
+        if (subItem.linkId === linkId) {
+          return (
+            subItem.answer?.[0]?.valueCoding?.code ||
+            subItem.answer?.[0]?.valueInteger ||
+            subItem.answer?.[0]?.valueString ||
+            "N/A"
+          );
         }
       }
     }
-    return null;
+    return "N/A";
   }
 
-  function getQuestionnaireItemByLinkId(linkId) {
-    const questionnaireItem = findQuestionnaireItemByLinkId(
-      Questionnaire.item,
-      linkId
-    );
-    return questionnaireItem || {}; // Return an empty object if not found to avoid errors
-  }
+  const overviewScreenFields = [
+    {
+      title: "Personal Information",
+      key: "personalInformation",
+      data: [
+        {
+          text: "Prename",
+          answer: registeredPatient.name[0].given[0],
+          key: "p.1.1",
+        },
+        {
+          text: "Surname",
+          answer: registeredPatient.name[0].family,
+          key: "p.1.2",
+        },
+        {
+          text: "Date of Birth",
+          answer: registeredPatient.birthDate,
+          key: "p.1.3",
+        },
+        {
+          text: "Insurance Number",
+          answer: registeredPatient.identifier[0].value,
+          key: "p.1.4",
+        },
+        { text: "Gender", answer: registeredPatient.gender, key: "p.1.5" },
+      ],
+    },
+    {
+      title: translatedQuestionnaireTexts["q.1"],
+      key: "questionnaire",
+      data: [
+        {
+          key: "q.1.1",
+          text: translatedQuestionnaireTexts["q.1.1"],
+          answer: findAnswerByLinkId("q.1.1"),
+        },
+        {
+          key: "q.1.2",
+          text: translatedQuestionnaireTexts["q.1.2"],
+          answer: findAnswerByLinkId("q.1.2"),
+        },
+        {
+          key: "q.1.3",
+          text: translatedQuestionnaireTexts["q.1.3"],
+          answer: findAnswerByLinkId("q.1.3"),
+        },
+      ],
+    },
+    {
+      title: translatedQuestionnaireTexts["q.2"],
+      key: "examinations",
+      data: [
+        {
+          key: "q.2.1",
+          text: translatedQuestionnaireTexts["q.2.1"],
+          answer: findAnswerByLinkId("q.2.1"),
+        },
+        {
+          key: "q.2.2",
+          text: translatedQuestionnaireTexts["q.2.2"],
+          answer: findAnswerByLinkId("q.2.2"),
+        },
+        {
+          key: "q.2.3",
+          text: translatedQuestionnaireTexts["q.2.3"],
+          answer: findAnswerByLinkId("q.2.3"),
+        },
+        {
+          key: "q.2.4",
+          text: translatedQuestionnaireTexts["q.2.4"],
+          answer: findAnswerByLinkId("q.2.4"),
+        },
+      ],
+    },
+    {
+      title: translatedQuestionnaireTexts["q.3"],
+      key: "medicalConditions",
+      data: [
+        {
+          key: "q.3.1",
+          text: translatedQuestionnaireTexts["q.3.1"],
+          answer: findAnswerByLinkId("q.3.1"),
+        },
+        {
+          key: "q.3.2",
+          text: translatedQuestionnaireTexts["q.3.2"],
+          answer: findAnswerByLinkId("q.3.2"),
+        },
+        {
+          key: "q.3.3",
+          text: translatedQuestionnaireTexts["q.3.3"],
+          answer: findAnswerByLinkId("q.3.3"),
+        },
+        {
+          key: "q.3.4",
+          text: translatedQuestionnaireTexts["q.3.4"],
+          answer: findAnswerByLinkId("q.3.4"),
+        },
+      ],
+    },
+    {
+      title: translatedQuestionnaireTexts["q.4"],
+      key: "medicationAndDevices",
+      data: [
+        {
+          key: "q.4.1",
+          text: translatedQuestionnaireTexts["q.4.1"],
+          answer: findAnswerByLinkId("q.4.1"),
+        },
+        {
+          key: "q.4.2",
+          text: translatedQuestionnaireTexts["q.4.2"],
+          answer: findAnswerByLinkId("q.4.2"),
+        },
+        {
+          key: "q.4.3",
+          text: translatedQuestionnaireTexts["q.4.3"],
+          answer: findAnswerByLinkId("q.4.3"),
+        },
+        {
+          key: "q.4.4",
+          text: translatedQuestionnaireTexts["q.4.4"],
+          answer: findAnswerByLinkId("q.4.4"),
+        },
+        {
+          key: "q.4.5",
+          text: translatedQuestionnaireTexts["q.4.5"],
+          answer: findAnswerByLinkId("q.4.5"),
+        },
+        {
+          key: "q.4.6",
+          text: translatedQuestionnaireTexts["q.4.6"],
+          answer: findAnswerByLinkId("q.4.6"),
+        },
+      ],
+    },
+    {
+      title: translatedQuestionnaireTexts["q.5"],
+      key: "additionalQuestions",
+      data: [
+        {
+          key: "q.5.1",
+          text: translatedQuestionnaireTexts["q.5.1"],
+          answer: findAnswerByLinkId("q.5.1"),
+        },
+        {
+          key: "q.5.2",
+          text: translatedQuestionnaireTexts["q.5.2"],
+          answer: findAnswerByLinkId("q.5.2"),
+        },
+      ],
+    },
+  ];
 
   function renderOverviewItem({ item }) {
-    const questionnaireItem = getQuestionnaireItemByLinkId(item.linkId);
-
-    const renderAnswer = () => {
-      switch (questionnaireItem.type) {
-        case "integer":
-          return (
-            <Text style={fontStyle.p}>{item.answer[0]?.valueInteger}</Text>
-          );
-        case "string":
-          return <Text style={fontStyle.p}>{item.answer[0]?.valueString}</Text>;
-        case "choice":
-          return (
-            <Text style={fontStyle.p}>{item.answer[0]?.valueCoding.code}</Text>
-          );
-        default:
-          return <Text>Unknown Type</Text>;
-      }
-    };
-
+    switch (item.answer) {
+      case "Y":
+        item.answer = translatedQuestionnaireTexts["q.yes"];
+        break;
+      case "N":
+        item.answer = translatedQuestionnaireTexts["q.no"];
+        break;
+      case "ASKU":
+        item.answer = translatedQuestionnaireTexts["q.idk"];
+        break;
+      case "male":
+        item.answer = translatedPatientTexts["p.male"];
+        break;
+      case "female":
+        item.answer = translatedPatientTexts["p.female"];
+        break;
+      case "other":
+        item.answer = translatedPatientTexts["p.other"];
+        break;
+      default:
+        item.answer = item.answer;
+    }
     return (
-      <View style={overviewStyle.overviewContainer}>
-        <Text style={fontStyle.p}>{questionnaireItem.text}</Text>
-        {renderAnswer()}
+      <View>
+        <Text style={fontStyle.p}>{item.text}</Text>
+        <Text style={fontStyle.p2}>{item.answer}</Text>
       </View>
     );
   }
 
-  function renderOverviewSectionHeader({ section: { title } }) {
-    return <Text style={fontStyle.h3}>{title}</Text>;
+  function renderOverviewSectionHeader({ section }) {
+    return (
+      <View style={fontStyle.h2}>
+        <Text style={fontStyle.h2}>{section.title}</Text>
+      </View>
+    );
   }
 
   return (
@@ -81,8 +234,9 @@ export default function OverviewScreen() {
       </View>
       <View style={commonStyle.section}>
         <SectionList
-          sections={updatedQuestionnaireResponseSections}
-          keyExtractor={(item) => item.linkId}
+          style={{ marginBottom: 30 }}
+          sections={overviewScreenFields}
+          keyExtractor={(item) => item.key}
           renderItem={renderOverviewItem}
           renderSectionHeader={renderOverviewSectionHeader}
           stickySectionHeadersEnabled={false}
