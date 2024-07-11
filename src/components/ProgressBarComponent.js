37e98c3101962e2387370ca771316d312a7cc53a
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-11 15:49:17 +0200

    Create ProgressBarComponent.js

---
+++ src/components/ProgressBarComponent.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import ProgressSteps, {
+  Title,
+  Content,
+} from "@joaosousa/react-native-progress-steps";
+import { medBuddyBlue } from "../styles/colors";
+
+export default function ProgressBarComponent({ currentStep }) {
+  return (
+    <View style={styles.container}>
+      <ProgressSteps
+        currentStep={currentStep}
+        orientation="horizontal"
+        steps={[
+          {
+            title: (
+              <Title>
+                <Text>Information</Text>
+              </Title>
+            ),
+            content: <Content></Content>,
+          },
+          {
+            title: (
+              <Title>
+                <Text>Questionnaire</Text>
+              </Title>
+            ),
+            content: <Content></Content>,
+          },
+          {
+            title: (
+              <Title>
+                <Text>Overview</Text>
+              </Title>
+            ),
+            content: <Content></Content>,
+          },
+          {
+            title: (
+              <Title>
+                <Text>Consent</Text>
+              </Title>
+            ),
+            content: <Content></Content>,
+          },
+        ]}
+        colors={{
+          title: {
+            text: {
+              normal: "lightgrey",
+              active: medBuddyBlue,
+              completed: medBuddyBlue,
+            },
+          },
+          marker: {
+            text: {
+              normal: "grey",
+              active: "#005f73",
+              completed: "#f4f3ee",
+            },
+            line: {
+              normal: "lightgrey",
+              active: medBuddyBlue,
+              completed: medBuddyBlue,
+            },
+          },
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "60%",
+  },
+});
