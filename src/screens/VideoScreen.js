a894e027c1b3b7f056fc6c4bf39ddf5a0a40baa3
mochsenhofer <123185718+mochsenhofer@users.noreply.github.com>
2024-07-11 09:51:14 +0200

    Update VideoScreen.js

--- src/screens/VideoScreen.js
+++ src/screens/VideoScreen.js
@@ -1,18 +1,75 @@
-import React from "react";
-import { View, SafeAreaView } from "react-native";
-import { commonStyle } from "../styles/commonStyle";
+import { useIsFocused } from "@react-navigation/native";
+import { Video } from "expo-av";
+import React, { useRef } from "react";
+import { SafeAreaView, SectionList, StyleSheet, View } from "react-native";
+import { useSelector } from "react-redux";
+import { textsInPatientsChosenLanguage } from "../assets/translationTexts/textsInPatientsChosenLanguage";
 import { BottomNavigationView } from "../components/BottomNavigationView";
+import renderSectionHeader from "../functions/renderSectionHeader";
 import { questionnaireScreenRoute } from "../navigation/Navigation";
-
+import { commonStyle } from "../styles/commonStyle";
+import MedBuddyCornerLogo from "../components/MedBuddyCornerLogo";
 
 export default function VideoScreen() {
+  const registeredPatient = useSelector((state) => state.patient);
+  const language = registeredPatient.communication[0].language.coding[0].code;
+  const translatedVideoTexts =
+    textsInPatientsChosenLanguage[language].videoScreen;
+  const videoRef = useRef(null);
+  const videoSrc = translatedVideoTexts.video;
+
+  const videoScreenSections = [
+    {
+      title: translatedVideoTexts["v.1"],
+      data: [
+        {
+          key: "videoSrc",
+          video: videoSrc,
+        },
+      ],
+    },
+  ];
+
+  function renderVideoItem({ item }) {
     return (
-        <SafeAreaView style={commonStyle.body}>
-            <View style={commonStyle.header}></View>
-            <View style={commonStyle.section}></View>
-            <View style={commonStyle.footer}>
-                <BottomNavigationView navigateTo={questionnaireScreenRoute}/>
-            </View>
-        </SafeAreaView>
+      <Video
+        ref={videoRef}
+        shouldPlay={useIsFocused()}
+        source={item.video}
+        style={styles.video}
+        isMuted={false}
+      />
     );
+  }
+
+  return (
+    <SafeAreaView style={commonStyle.body}>
+      <View style={commonStyle.header}>
+        <MedBuddyCornerLogo />
+      </View>
+      <View style={[commonStyle.section, styles.center]}>
+        <SectionList
+          sections={videoScreenSections}
+          keyExtractor={(item, index) => item + index}
+          renderItem={renderVideoItem}
+          renderSectionHeader={renderSectionHeader}
+        />
+      </View>
+      <View style={commonStyle.footer}>
+        <BottomNavigationView navigateTo={questionnaireScreenRoute} />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  video: {
+    width: "100%",
+    height: "auto",
+    aspectRatio: 1280 / 720,
+  },
+  center: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
